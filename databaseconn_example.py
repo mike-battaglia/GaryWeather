83dd7780046fed68d9ee77123fe886dcba435ef4
MBATT2321 <mike@mikebattaglia.com>
2021-05-04 19:44:51 -0500

    Changed primarykey

--- databaseconn_example.py
+++ databaseconn_example.py
@@ -2,54 +2,40 @@ import mysql.connector
 import bme280_sensor
 import time
 import importlib
+import datetime
 
 cnx = mysql.connector.connect(
+  host = "domain.com",
+  database = "database",
+  user = "user",
+  password = "password"
 )
 
-# print(cnx)
+primarykey = datetime.datetime.now()
+temperature = bme280_sensor.temperature
+humidity = bme280_sensor.humidity
+pressure = bme280_sensor.pressure
+feelslike = bme280_sensor.feels
+dewpoint = bme280_sensor.dewpoint
 
-# while True:
-#  time.sleep(3)
-#  print(bme280_sensor.today)
-#  print(bme280_sensor.current_time)
-#  print(bme280_sensor.temperature)
-#  print(bme280_sensor.pressure)
-#  print(bme280_sensor.humidity)
-#  print(bme280_sensor.feels)
-#  print(bme280_sensor.dewpoint)
-#  time.sleep(6)
-#  importlib.reload(bme280_sensor)
-#  time.sleep(3)
+cursor = cnx.cursor()
+sql = "INSERT INTO weather_data (datetime, temperature, humidity, pressure, feels, dewpoint) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+val = (primarykey, temperature, humidity, pressure, feelslike, dewpoint)
 
-def send2db():
-  date = bme280_sensor.today
-  ctime = bme280_sensor.current_time
-  temperature = bme280_sensor.temperature
-  humidity = bme280_sensor.humidity
-  pressure = bme280_sensor.pressure
-  feelslike = bme280_sensor.feels
-  dewpoint = bme280_sensor.dewpoint
-  print(ctime)
-  cursor = cnx.cursor()
-  sql = "INSERT INTO weather_data (date, time, temperature, humidity, pressure, feels, dewpoint) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-  val = (date, ctime, temperature, humidity, pressure, feelslike, dewpoint)
+def send_to_db():
   cursor.execute(sql, val)
   cnx.commit()
   cursor.close()
   cnx.close()
-  time.sleep(60)
-  importlib.reload(bme280_sensor)
-#  print(date, time)
-#  print("T: ", temperature, "F")
-#  print("H: ", humidity, "%")
-#  print("P: ", pressure, "mb")
-#  print("F:", feelslike, "F")
-#  print("D:", dewpoint, "F")
 
+def print_all_the_things():
+  print("Datetime:", primarykey)
+  print("Temp:", temperature)
+  print("Humidity:", humidity)
+  print("Pressure:", pressure)
+  print("Feelslike:", feelslike)
+  print("Dewpoint:", dewpoint)
 
-#while True:
-#  time.sleep(3)
-#  send2db()
-#  time.sleep(3)
+# send_to_db()
 
-send2db()
+# print_all_the_things
